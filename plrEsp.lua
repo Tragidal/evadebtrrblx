3ae2173c935d2b866c3c05bbfc3733e310ba6075
Tragidal <67218546+Tragidal@users.noreply.github.com>
2023-03-05 19:50:50 -0600

    Update plrEsp.lua

--- plrEsp.lua
+++ plrEsp.lua
@@ -1,92 +1,87 @@
-plrESP = function()
-		local c = workspace.CurrentCamera
-		local ps = game:GetService("Players")
-		local lp = ps.LocalPlayer
-		local rs = game:GetService("RunService")
-		local function getdistancefc(part)
-			return (part.Position - c.CFrame.Position).Magnitude
-		end
-		local function esp(p, cr)
-			local h = cr:WaitForChild("Humanoid")
-			local hrp = cr:WaitForChild("HumanoidRootPart")
-			local text = Drawing.new("Text")
-			text.Visible = false
-			text.Center = true
-			text.Outline = true
-			text.Font = 2
-			text.Color = Color3.fromRGB(255, 255, 255)
-			text.Size = 17
-			local c1
-			local c2
-			local c3
-			local function dc()
-				text.Visible = false
-				text:Remove()
-				if c1 then
-					c1:Disconnect()
-					c1 = nil
-				end
-				if c2 then
-					c2:Disconnect()
-					c2 = nil
-				end
-				if c3 then
-					c3:Disconnect()
-					c3 = nil
-				end
-			end
-			c2 =
-				cr.AncestryChanged:Connect(
-				function(_, parent)
-					if not parent then
-						dc()
-					end
-				end
-			)
-			c3 =
-				h.HealthChanged:Connect(
-				function(v)
-					if (v <= 0) or (h:GetState() == Enum.HumanoidStateType.Dead) then
-						dc()
-					end
-				end
-			)
-			c1 =
-				rs.RenderStepped:Connect(
-				function()
-					local hrp_pos, hrp_os = c:WorldToViewportPoint(hrp.Position)
-					if hrp_os then
-						text.Position = Vector2.new(hrp_pos.X, hrp_pos.Y)
-						text.Text = p.Name .. " (" .. tostring(math.floor(getdistancefc(hrp))) .. " m)"
-						text.Visible = true
-					else
-						text.Visible = false
-					end
-				end
-			)
-		end
-		local function p_added(p)
-			if p.Character then
-				esp(p, p.Character)
-			end
-			p.CharacterAdded:Connect(
-				function(cr)
-					esp(p, cr)
-				end
-			)
-		end
-		for i, p in next, ps:GetPlayers() do
-			if p ~= lp then
-				p_added(p)
-			end
-		end
-		ps.PlayerAdded:Connect(p_added)
-        OrionLib:MakeNotification({
-			Name = "Hydra Network",
-			Content = "Pressed on the Player Esp Button",
-			Image = "rbxassetid://4483345998",
-			Time = 2
-		}) 
-  	end
+local esp_settings = {
+   textsize = 16,
+   colour = 255,255,255
+}
 
-plrESP()
+local gui = Instance.new("BillboardGui")
+local esp = Instance.new("TextLabel",gui)
+
+local plr = game.Players.LocalPlayer
+
+gui.Name = "ESP";
+gui.ResetOnSpawn = false
+gui.AlwaysOnTop = true;
+gui.LightInfluence = 0;
+gui.Size = UDim2.new(1.75, 0, 1.75, 0);
+esp.BackgroundColor3 = Color3.fromRGB(255, 255, 255);
+esp.Text = "Nextbot"
+esp.Size = UDim2.new(0.0001, 0.00001, 0.0001, 0.00001);
+esp.BorderSizePixel = 4;
+esp.BorderColor3 = Color3.new(esp_settings.colour)
+esp.BorderSizePixel = 0
+esp.Font = "GothamSemibold"
+esp.TextSize = esp_settings.textsize
+esp.TextColor3 = Color3.fromRGB(esp_settings.colour)
+
+_G.Active = true
+
+game:GetService("RunService").RenderStepped:Connect(function()
+   for i,v in pairs (workspace.Game.Players:GetDescendants()) do
+       if _G.Active and v:FindFirstChild("HRP") and v.HRP and v.HRP:FindFirstChild("ESP") == nil then
+           gui:Clone().Parent = v.HRP
+end
+   end
+end)
+
+-- Drawing Lib
+
+task.wait(.5)
+
+local lines =  {
+   
+}
+
+for i,v in pairs(workspace.Game.Players:GetDescendants()) do
+   if v.Name == "ESP" and _G.Active then
+       local camera = workspace.Camera
+
+       local Line = Drawing.new("Line")
+
+       table.insert(lines, {Line = Line, part = v.Parent})
+
+       Line.From = Vector2.new(camera.ViewportSize.X/2, camera.ViewportSize.Y)
+       Line.Color = Color3.fromRGB(255, 255, 255)
+       Line.Thickness = 2
+   end
+end
+
+workspace.Game.Players.DescendantAdded:Connect(function(v)
+   if v.Name == "ESP" and _G.Active then
+       local camera = workspace.Camera
+       local Line = Drawing.new("Line")
+
+       table.insert(lines, {Line = Line, part = v.Parent})
+
+       Line.From = Vector2.new(camera.ViewportSize.X/2, camera.ViewportSize.Y)
+       Line.Color = Color3.fromRGB(255, 255, 255)
+       Line.Thickness = 2
+   end
+end)
+
+game:GetService("RunService").RenderStepped:Connect(function()
+   for i,v in pairs(lines) do
+       if v.part ~= nil and _G.Active then
+           local camera = workspace.Camera
+           local v2, inViewport = camera:WorldToViewportPoint(v.part.Position)
+           v.Line.To = Vector2.new(v2.X, v2.Y)
+           v.Line.From = Vector2.new(camera.ViewportSize.X/2, camera.ViewportSize.Y)
+
+           v.Line.Visible = (inViewport and v.part and v.part.Parent and true) or false
+
+           if v.part == nil then
+               v.Line.Visible = false
+               v.Line = nil
+           end
+       end
+   end
+end)
